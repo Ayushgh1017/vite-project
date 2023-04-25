d4f55e6eeff57216402295cfa5680feaef961dee
Ayush Ghildiyal <ghildiyala5@gmail.com>
2023-04-25 22:55:07 +0530

    commit message

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -68,7 +68,7 @@ const ContactUs = ({ setSelectedPage }: Props) => {
             <form
               target="_blank"
               onSubmit={onSubmit}
-              action="https://formsubmit.co/e8a5bdfa807605332f809e5656e27c6e"
+              action="https://formsubmit.co/3a4dc3dbe980902e0124568408593fa5"
               method="POST"
             >
               <input
